59b2e85c1a2a98484953b14e700d818cdf118081
MARCUS SANTOS <ctl_marsantos@uolinc.com>
2022-11-21 15:22:26 -0300

    Add new bucket teste region

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
 resource "google_storage_bucket" "gcs_bucket" {
   name = "test-bucket-randon-144124"
 }
+
+resource "google_storage_bucket" "gcs_buckte_2" {
+  name = "novo-teste-bucket-ms"
+  location = var.region
+}
