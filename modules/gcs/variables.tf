5de9894eb500d1cb0e7a476afa65d89115b395b5
MARCUS SANTOS <ctl_marsantos@uolinc.com>
2022-11-21 22:19:58 -0300

    Fix Modules

--- modules/gcs/variables.tf
+++ modules/gcs/variables.tf
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 variable "name" {
   description = "The name of the bucket."
   type        = string
